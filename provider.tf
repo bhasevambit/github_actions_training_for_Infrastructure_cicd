2377ac1f8a29dedbfc78afd828c4df06e478f55a
bhasevambit <jammadoibit6123@gmail.com>
2023-03-25 13:58:45 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
 provider "aws" {
   region  = "ap-northeast-1"
   version = "2.49.0"
+
+  default_tags {
+    tags = {
+      Managed   = "terraform"
+      Reference = "basic_github_actions"
+    }
+  }
 }
